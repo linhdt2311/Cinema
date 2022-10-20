99e7fca1ed323121fabf4d5fdbe7976325c87918
Đặng Linh <104380101+linhdepzai@users.noreply.github.com>
2022-10-20 19:44:29 +0700

    create, edit, delete

--- API/Cinema/Controllers/RoomController.cs
+++ API/Cinema/Controllers/RoomController.cs
@@ -29,15 +29,6 @@ namespace Cinema.Controllers
                 Room room = new Room(i);
                 roomList.Add(room);
             }
-            //SqlDataReader reader = sqlCommand.ExecuteReader();
-            /*while (reader.Read())
-            {
-                var room = new Room();
-                room.RId = (int)reader["RId"];
-                room.RName = (int)reader["RName"];
-                room.RStatus = (int)reader["RStatus"];
-                roomList.Add(room);
-            }*/
             conn.Close();
             return roomList.ToList();
         }
@@ -45,10 +36,8 @@ namespace Cinema.Controllers
         public bool CreateRoom(int name, int creatorUserId)
         {
             conn.Open();
-            string sql = string.Format("exec CreateRoom @CreatorUserId = _CreatorUserId, @RName = _RName");
+            string sql = string.Format("exec CreateRoom @CreatorUserId = " + creatorUserId + ", @RName = " + name);
             SqlCommand sqlCommand = new SqlCommand(sql, conn);
-            sqlCommand.Parameters.AddWithValue("_CreatorUserId", creatorUserId);
-            sqlCommand.Parameters.AddWithValue("_RName", name);
             if (sqlCommand.ExecuteNonQuery() > 0) return true;
             conn.Close();
             return false;
@@ -57,13 +46,8 @@ namespace Cinema.Controllers
         public bool UpdateRoom(int rId, int rName, int rStatus, int lastModifierUserId)
         {
             conn.Open();
-            string sql = string.Format("exec UpdateRoom @LastModifierUserId = _LastModifierUserId, @RId = _RId, @RName = _RName, @RStatus = _RStatus");
+            string sql = string.Format("exec UpdateRoom @LastModifierUserId = " + lastModifierUserId + ", @RId = " + rId + ", @RName = " + rName + ", @RStatus = " + rStatus);
             SqlCommand sqlCommand = new SqlCommand(sql, conn);
-            sqlCommand.Parameters.AddWithValue("_LastModifierUserId", lastModifierUserId);
-            sqlCommand.Parameters.AddWithValue("_RId", rId);
-            sqlCommand.Parameters.AddWithValue("_RName", rName);
-            sqlCommand.Parameters.AddWithValue("_RStatus", rStatus);
-
             if (sqlCommand.ExecuteNonQuery() > 0) return true;
             conn.Close();
             return false;
@@ -72,12 +56,9 @@ namespace Cinema.Controllers
         public bool DeleteRoom(int rId, int deleterUserId)
         {
             conn.Open();
-            string sql = string.Format("exec DeleteRoom @DeleterUserId = _deleterUserId, @RId = _RId");
+            string sql = string.Format("exec DeleteRoom @DeleterUserId = " + deleterUserId + ", @RId = " + rId);
 
             SqlCommand sqlCommand = new SqlCommand(sql, conn);
-            sqlCommand.Parameters.AddWithValue("_DeleterUserId", deleterUserId);
-            sqlCommand.Parameters.AddWithValue("_RId", rId);
-
             if (sqlCommand.ExecuteNonQuery() > 0) return true;
             conn.Close();
             return false;

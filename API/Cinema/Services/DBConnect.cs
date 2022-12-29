b5ed8f742c91a648bd3a5f4e71373dfe1e72c4cf
Đặng Linh <104380101+linhdepzai@users.noreply.github.com>
2022-12-29 17:17:14 +0700

    save

--- API/Cinema/Services/DBConnect.cs
+++ API/Cinema/Services/DBConnect.cs
@@ -1,8 +1,9 @@
-﻿using System.Data.SqlClient;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace Cinema.Services
 {
-    public class DBConnect
+    public class DBConnect : ControllerBase
     {
         //public SqlConnection conn = new SqlConnection("Data Source=LINH\\SQL2019;Initial Catalog=Cinema;Integrated Security=True;User ID=sa;pwd=linh123456");
         //public SqlConnection conn = new SqlConnection("Data Source=DESKTOP-SI1SCKG;Initial Catalog=Cinema;Integrated Security=True;User ID=sa;pwd=456789asd");
